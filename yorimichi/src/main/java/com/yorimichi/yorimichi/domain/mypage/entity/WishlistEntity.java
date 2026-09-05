b12d7a2cf416b3427182e18035d090b0e54022a4
유영수 <154852358+lukdown@users.noreply.github.com>
2026-09-05 22:05:40 +0900

    Revert "Merge branch 'master' of https://github.com/TeamYoRiMiChi/YoRiMiChi_back"
    This reverts commit 45dc9f9f27c7cdcce791b0c85b2161bdcffd6e64, reversing
    changes made to 7b00734136115b3a18d50d5b95e7740dceb092f0.

---
+++ yorimichi/src/main/java/com/yorimichi/yorimichi/domain/mypage/entity/WishlistEntity.java
@@ -0,0 +1,5 @@
+package com.yorimichi.yorimichi.domain.mypage.entity;
+
+public class WishlistEntity {
+
+}
--- yorimichi/src/main/java/com/yorimichi/yorimichi/domain/wishlist/controller/WishlistController.java
+++
@@ -1,5 +0,0 @@
-package com.yorimichi.yorimichi.domain.wishlist.controller;
-
-public class WishlistController {
-
-}
