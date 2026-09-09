efcfad20432d2d20da42573396fb67f8d70fe5c5
Yun Seokmin <epictune39@gmail.com>
2026-09-09 16:21:31 +0900

    Mypage_cart

--- yorimichi/src/main/java/com/yorimichi/yorimichi/domain/mypage/service/MypageCartService.java
+++ yorimichi/src/main/java/com/yorimichi/yorimichi/domain/mypage/service/MypageCartService.java
@@ -1,5 +1,27 @@
 package com.yorimichi.yorimichi.domain.mypage.service;
 
-public class MypageCartService {
+import java.util.List;
 
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.yorimichi.yorimichi.domain.mypage.dto.MypageCartResponseDto;
+import com.yorimichi.yorimichi.domain.mypage.repository.MypageCartMapper;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class MypageCartService {
+	
+	private final MypageCartMapper mypageCartMapper;
+	
+	//로그인 회원의 장바구니 상품 목록 조회
+	@Transactional(readOnly = true)
+	public List<MypageCartResponseDto> getMypageCart(Long memberId){
+		return mypageCartMapper.findAllByMemberId(memberId)
+				.stream()
+				.map(MypageCartResponseDto::new)
+				.toList();
+	}
 }
